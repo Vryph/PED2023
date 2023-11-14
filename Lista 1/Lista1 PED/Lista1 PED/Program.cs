7103760e8e4f08b8a2d6ad00746bcb6c0eafe6af
Vryph <133055968+Vryph@users.noreply.github.com>
2023-11-14 11:59:42 -0300

    Menu
    Implementei o menu

--- Lista 1/Lista1 PED/Lista1 PED/Program.cs
+++ Lista 1/Lista1 PED/Lista1 PED/Program.cs
@@ -1,86 +1,111 @@
 ﻿using Lista1_PED;
 
-//Teste dos Nodes -------------------------
-MyNode<float> node1 = new MyNode<float>(1f);
-MyNode<float> node2 = new MyNode<float>(2f);
-MyNode<float> node3 = new MyNode<float>(3f);
+Menu();
 
-node1.InsertAfter(node2);
-node3.InsertBefore(node1);
+void Menu()
+{
+    Console.Clear();
+    Console.WriteLine($"Escolha uma das opcoes dos exercícios: ");
+    Console.WriteLine($"1 - Testes das estruturas de dados");
+    Console.WriteLine($"2 - Inventário");
+    Console.WriteLine($"3 - Torre de Hanoi");
+    Console.WriteLine($"4 - Simulador de Boliche");
+    string opcao = Console.ReadLine();
+    if(opcao == "1") { Testes(); }
+    else { opcao = "0";  Menu(); }
+}
+void Testes() {
+    Console.Clear();
+    //Teste dos Nodes -------------------------
+    MyNode<float> node1 = new MyNode<float>(1f);
+    MyNode<float> node2 = new MyNode<float>(2f);
+    MyNode<float> node3 = new MyNode<float>(3f);
 
-Console.WriteLine("Node  2 - 3 - 1");
-Console.Write($"{ node1.Previous.Previous.value}  - ");
-Console.Write($"{node1.Previous.value} - ");
-Console.WriteLine($"{node3.Next.value}");
+    node1.InsertAfter(node2);
+    node3.InsertBefore(node1);
 
-node3.Detach();
+    Console.WriteLine("Node  2 - 3 - 1");
+    Console.Write($"{node1.Previous.Previous.value}  - ");
+    Console.Write($"{node1.Previous.value} - ");
+    Console.WriteLine($"{node3.Next.value}");
 
-Console.WriteLine("Node  2 - 1");
-Console.Write($"{node1.Previous.value}  - ");
-Console.WriteLine($"{node2.Next.value}");
+    node3.Detach();
 
-//Teste da Lista ----------------------------
-Console.WriteLine();
-Console.WriteLine();
+    Console.WriteLine("Node  2 - 1");
+    Console.Write($"{node1.Previous.value}  - ");
+    Console.WriteLine($"{node2.Next.value}");
 
-MyLinkedList<float> myList = new MyLinkedList<float>();
-myList.Append(1);
-myList.Append(2);
-myList.Append(3);
-myList.Preppend(4);
-myList.Preppend(5);
+    //Teste da Lista ----------------------------
+    Console.WriteLine();
+    Console.WriteLine();
 
-Console.WriteLine($"Valor no index 3 = {myList.Get(3)}");
-myList.Print();
-Console.WriteLine($"Contagem da Lista: {myList.Count()}");
-Console.WriteLine();
+    MyLinkedList<float> myList = new MyLinkedList<float>();
+    myList.Append(1);
+    myList.Append(2);
+    myList.Append(3);
+    myList.Preppend(4);
+    myList.Preppend(5);
 
-myList.Append(3);
-Console.WriteLine($"Existe 2? {myList.Contains(2)}  -  Existe 6? {myList.Contains(6)}");
-Console.WriteLine($"Remover o valor 3 e então o índice 1");
-myList.Print();
-myList.Remove(3);
-myList.Print();
-myList.RemoveAt(1);
-myList.Print();
+    Console.WriteLine($"Valor no index 3 = {myList.Get(3)}");
+    myList.Print();
+    Console.WriteLine($"Contagem da Lista: {myList.Count()}");
+    Console.WriteLine();
 
-Console.WriteLine("Limpando a Lista...");
-myList.Clear();
-myList.Print();
+    myList.Append(3);
+    Console.WriteLine($"Existe 2? {myList.Contains(2)}  -  Existe 6? {myList.Contains(6)}");
+    Console.WriteLine($"Remover o valor 3 e então o índice 1");
+    myList.Print();
+    myList.Remove(3);
+    myList.Print();
+    myList.RemoveAt(1);
+    myList.Print();
 
-//Teste da Pilha ---------------------------------------------------------------
-Console.WriteLine();
-Console.WriteLine();
+    Console.WriteLine("Limpando a Lista...");
+    myList.Clear();
+    myList.Print();
 
-MyStack<float> myStack = new MyStack<float>();
-myStack.Push(10);
-myStack.Push(20);
-myStack.Push(30);
-Console.WriteLine($"Número de Valores na Pilha {myStack.Count()}");
-Console.WriteLine($"{myStack.Pop()} removido");
-Console.WriteLine($"Topo da Pilha = {myStack.Peek()}");
+    //Teste da Pilha ---------------------------------------------------------------
+    Console.WriteLine();
+    Console.WriteLine();
 
-Console.WriteLine("Limpando a lista");
-myStack.Clear();
-Console.WriteLine($"Topo da Pilha = {myStack.Peek()}");
+    MyStack<float> myStack = new MyStack<float>();
+    myStack.Push(10);
+    myStack.Push(20);
+    myStack.Push(30);
+    Console.WriteLine($"Número de Valores na Pilha {myStack.Count()}");
+    Console.WriteLine($"{myStack.Pop()} removido");
+    Console.WriteLine($"Topo da Pilha = {myStack.Peek()}");
 
-//Teste Fila ---------------------------------------------------------------------
-Console.WriteLine();
-Console.WriteLine();
+    Console.WriteLine("Limpando a lista");
+    myStack.Clear();
+    Console.WriteLine($"Topo da Pilha = {myStack.Peek()}");
 
-MyQueue<float> myQueue = new MyQueue<float>();
-myQueue.Enqueue(5);
-myQueue.Enqueue(10);
-myQueue.Enqueue(15);
-myQueue.Enqueue(30);
+    //Teste Fila ---------------------------------------------------------------------
+    Console.WriteLine();
+    Console.WriteLine();
 
-Console.WriteLine($"Número de Valores na Fila = {myQueue.Count()}");
-Console.WriteLine($"Primeiro valor = {myQueue.First()}");
-Console.WriteLine($"Último valor = {myQueue.Last()}");
-myQueue.Dequeue();
-Console.WriteLine($"Dequeue: Primeiro valor = {myQueue.First()}");
-myQueue.Clear();
-Console.WriteLine($"Clear: Primeiro valor = {myQueue.First()}, Último valor = {myQueue.Last()}");
+    MyQueue<float> myQueue = new MyQueue<float>();
+    myQueue.Enqueue(5);
+    myQueue.Enqueue(10);
+    myQueue.Enqueue(15);
+    myQueue.Enqueue(30);
 
+    Console.WriteLine($"Número de Valores na Fila = {myQueue.Count()}");
+    Console.WriteLine($"Primeiro valor = {myQueue.First()}");
+    Console.WriteLine($"Último valor = {myQueue.Last()}");
+    myQueue.Dequeue();
+    Console.WriteLine($"Dequeue: Primeiro valor = {myQueue.First()}");
+    myQueue.Clear();
+    Console.WriteLine($"Clear: Primeiro valor = {myQueue.First()}, Último valor = {myQueue.Last()}");
 
+    //Teste de Iteradores
+    Console.WriteLine();
+    Console.WriteLine();
+
+    //Fim-------------------------
+    Console.WriteLine("Aperte Enter para voltar ao Menu.");
+    string retorno = "0";
+    retorno = Console.ReadLine();
+    if(retorno != null) { retorno = "0"; Menu(); }
+}
 
